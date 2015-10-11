e2a8ad7910ccc5438c7c6dc447721d1ba9fef665
Jacob Huesman <jakehuesman@gmail.com>
2015-10-11 01:53:57 -0500

    joy_turret functional

--- src/turret/scripts/joy_turret.py
+++ src/turret/scripts/joy_turret.py
@@ -16,13 +16,13 @@ from std_msgs.msg import Float64
 
 # Rotate (parameters will change when servo is swapped out for mx-12a)
 # middle 510, max right 0, max left 1023
-maxLeft = 1023
-maxRight = 0
+maxLeft = 2
+maxRight = -2
 
 # Tilt: all the way down is pos 210, straight is pos 510, up is pos 810
-tiltUpperLimit = 810
-tiltLowerLimit = 210
-maxSpeed =  1.0
+tiltUpperLimit = 1
+tiltLowerLimit = -1
+maxSpeed =  4.0
 minSpeed = 0.0
 
 pan_pos = 0.0
@@ -38,6 +38,8 @@ def callback(data):
     #rospy.wait_for_service('/head_pan_joint/set_speed')
     #rospy.wait_for_service('/head_tilt_join/set_speed')
 
+    pan_pub = rospy.Publisher('head_pan_joint/command', Float64, queue_size=10)
+    tilt_pub = rospy.Publisher('head_tilt_joint/command', Float64, queue_size=10)
 
     # Relax
     if data.buttons[1] == 1:
@@ -52,6 +54,7 @@ def callback(data):
         except rospy.ServiceException, e:
             print "Service call failed: %s" %e
 
+    # Enable
     if data.buttons[2] == 1:
         rospy.wait_for_service('head_pan_joint/enable')
         rospy.wait_for_service('head_tilt_joint/enable')
@@ -74,48 +77,47 @@ def callback(data):
             set_tilt = rospy.ServiceProxy('head_tilt_joint/set_speed', SetSpeed)
             resp_pan = set_pan(maxSpeed)
             resp_tilt = set_tilt(maxSpeed)
-            print resp_pan
-            print resp_tilt
         except rospy.ServiceException, e:
             print "Service call failed: %s" %e
 
-        pan_pub = rospy.Publisher('head_pan_joint/command', Float64, queue_size=10)
-        tilt_pub = rospy.Publisher('head_tilt_joint/command', Float64, queue_size=10)
         rate = rospy.Rate(10)
         while ((pan_pos >= 0.1 or pan_pos <= -0.1) or (tilt_pos >= 0.1 or tilt_pos <= -0.1)):
             pan_pub.publish(0.0)
             tilt_pub.publish(0.0)
         print "Zeroed"
 
-'''
-    # Set speed
-    turret.setSpeed(1, int(abs(data.axes[3])*maxSpeed))
-    turret.setSpeed(2, int(abs(data.axes[4])*maxSpeed))
-	
-	# Rotate Turret
-	if data.axes[3] > 0.05 :
-		turret.setPosition(1, maxRight)
-	elif data.axes[3] < -0.05 :
-		turret.setPosition(1, maxLeft)
-	else:
-		turret.setPosition(1, turret.getPosition(1))
+    try:
+        set_pan = rospy.ServiceProxy('head_pan_joint/set_speed', SetSpeed)
+        set_tilt = rospy.ServiceProxy('head_tilt_joint/set_speed', SetSpeed)
+        resp_pan = set_pan(abs((data.axes[3])*maxSpeed))
+        resp_tilt = set_tilt(abs((data.axes[4])*maxSpeed))
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" %e
 
-	# Tilt Turret
-	if data.axes[4] > 0.1 :
-		turret.setPosition(2, tiltUpperLimit)
-	elif data.axes[4] < -0.1 :
-		turret.setPosition(2, tiltLowerLimit)
-	else:
-		turret.setPosition(2, turret.getPosition(2))
+    # Rotate Turret
+    if data.axes[3] > 0.05 :
+        pan_pub.publish(maxRight)
+    elif data.axes[3] < -0.05 :
+        pan_pub.publish(maxLeft)
+    else:
+        pan_pub.publish(pan_pos)
 
-'''
+    # Tilt Turret
+    if data.axes[4] > 0.1 :
+        tilt_pub.publish(tiltUpperLimit)
+    elif data.axes[4] < -0.1 :
+        tilt_pub.publish(tiltLowerLimit)
+    else:
+        tilt_pub.publish(tilt_pos)
+
+
 
 def listener():
 
     # Create a node called listener
     rospy.init_node('listener', anonymous=True)
 
-	# Subscribe to topic joy
+    # Subscribe to topic joy
     rospy.Subscriber("joy", Joy, callback)
 
     # Subscribe to joint states
